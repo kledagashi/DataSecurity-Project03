9da1211a41e2afb1c510371de13fb51a96bdfe5b
KlajdiGashi <klajdi.gashi@student.uni-pr.edu>
2023-05-13 19:14:26 +0200

    Added the server.java and the database

---
+++ src/src/Server.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.*;
+
+public class Server {
+    private static final int PORT = 8080;
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/mydatabase";
+    private static final String DB_USER = "username";
+    private static final String DB_PASSWORD = "password";
+
+    public static void main(String[] args) {
+
+    }
+
+}
