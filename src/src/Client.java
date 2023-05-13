9d8e3153a02594aaf5eb4463926aedfe26ccfd51
KlajdiGashi <klajdi.gashi@student.uni-pr.edu>
2023-05-13 19:42:48 +0200

    Added the Client code

--- src/src/Client.java
+++ src/src/Client.java
@@ -1,7 +1,17 @@
-import java.io.IOException;
+import java.io.*;
+import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) throws IOException {
+        Socket socket=new Socket("localhost",1234);
+        PrintWriter printWriter =new PrintWriter(socket.getOutputStream());
+        printWriter.println("is it working!");
+        printWriter.flush();
+
+        InputStreamReader Input=new InputStreamReader(socket.getInputStream());
+        BufferedReader bufferedReader=new BufferedReader(Input);
+        String Text=bufferedReader.readLine();
+        System.out.println("Server:"+Text);
 
     }
 }
