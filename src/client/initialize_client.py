d6e6813b71e8f15382c066886338ff2b47fdde91
Krenare158 <122061859+Krenare158@users.noreply.github.com>
2023-05-17 05:53:16 +1400

    Update initialize_client.py

--- src/client/initialize_client.py
+++ src/client/initialize_client.py
@@ -1,8 +1,6 @@
-import base64
-import os
 from tkinter import *
-
 import pyDes
+
 from socket import *
 import _thread
 
@@ -12,6 +10,7 @@ def initialize_client():
     s = socket(AF_INET, SOCK_STREAM)
     host = 'localhost'
     port = 1234
+
     s.connect((host, port))
 
     return s
