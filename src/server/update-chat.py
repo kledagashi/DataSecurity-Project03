a0ea681b5171a58e6e5483789c7034a298194415
Gentrit851 <53408143+Gentrit851@users.noreply.github.com>
2023-05-16 19:28:23 +0200

    add-update-chat-function

---
+++ src/server/update-chat.py
@@ -0,0 +1,11 @@
+def update_chat(msg, state):
+    global chatlog
+    chatlog.config(state=NORMAL)
+  
+    if state == 0:
+        chatlog.insert(END, msg)
+    else:
+        chatlog.insert(END, msg)
+    chatlog.config(state=DISABLED)
+   
+    chatlog.yview(END)
