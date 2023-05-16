460e8eba51d28d41b0475e7eefc2d3c55448c5bc
Kleda <118850687+kledagashi@users.noreply.github.com>
2023-05-16 17:14:01 +0200

    Update gui.py

--- src/client/gui.py
+++ src/client/gui.py
@@ -1,1 +1,24 @@
 
+def press(event):
+    send()
+
+def GUI():
+    global chatlog
+    global textbox
+
+
+    gui = Tk()
+    
+    gui.title("Client Chat")
+   
+    gui.geometry("400x430")
+
+    chatlog = Text(gui, bg='white')
+    chatlog.config(state=DISABLED)
+
+    sendbutton = Button(gui, bg='orange', fg='red', text='SEND', command=send)
+  
+    textbox = Text(gui, bg='white')
+
+    
+
