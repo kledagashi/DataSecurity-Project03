702dbb79e0d6cb5537b63d8612f670d82bb47cfc
Kleda <118850687+kledagashi@users.noreply.github.com>
2023-05-16 17:15:13 +0200

    updated gui.py

--- src/client/gui.py
+++ src/client/gui.py
@@ -19,6 +19,22 @@ def GUI():
     sendbutton = Button(gui, bg='orange', fg='red', text='SEND', command=send)
   
     textbox = Text(gui, bg='white')
+    
+    chatlog.place(x=6, y=6, height=386, width=390)
+    textbox.place(x=6, y=401, height=20, width=265)
+    sendbutton.place(x=300, y=401, height=20, width=50)
+   
+    textbox.bind("<KeyRelease-Return>", press)
+
+    _thread.start_new_thread(receive, ())
+    gui.mainloop()
+
+
+if __name__ == '__main__':
+    chatlog = textbox = None
+    s = initialize_client()
+    GUI()
+
 
     
 
