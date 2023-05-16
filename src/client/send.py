b00ddacd9038d9693b0ed2fc007b97de97642d47
Krenare158 <122061859+Krenare158@users.noreply.github.com>
2023-05-17 05:51:01 +1400

    Update send.py

--- src/client/send.py
+++ src/client/send.py
@@ -1,9 +1,12 @@
 def send():
     global textbox
+
     msg = textbox.get("0.0", END)
-    encrypted_data = createDES().encrypt(msg)
+    encrypted_data = readDES().encrypt(msg)
+
 
     update_chat(encrypted_data, 0)
+
 
     s.send(encrypted_data)
     textbox.delete("0.0", END)
