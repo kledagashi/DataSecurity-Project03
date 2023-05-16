485fda64729e65de98fc41d14557a6cc1e7170a5
Krenare158 <122061859+Krenare158@users.noreply.github.com>
2023-05-17 05:26:04 +1400

    Create send.py

---
+++ src/client/send.py
@@ -0,0 +1,9 @@
+def send():
+    global textbox
+    msg = textbox.get("0.0", END)
+    encrypted_data = createDES().encrypt(msg)
+
+    update_chat(encrypted_data, 0)
+
+    s.send(encrypted_data)
+    textbox.delete("0.0", END)
